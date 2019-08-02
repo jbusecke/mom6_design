1fa1f589625627b7d18d1fd09b5c2b2f91903b44
julius busecke <jbusecke@princeton.edu>
2019-08-02 08:56:16 -0400

    advanced refactor of postpr

--- mom6_design/postprocessing.py
+++ mom6_design/postprocessing.py
@@ -11,25 +11,38 @@ def plot_all(ds, **kwargs):
 def plot_cut(ds):
     return ds.squeeze().isel(zaxis_1=0, zaxis_2=0, aaxis_2=0).drop('H_to_kg_m2')
 
+# Merging restart files
+def combine_mom6_restarts(file_name):
+    datasets = []
+    for nrange in [range(0,4), range(4,8), range(8,12), range(12,16)]:
+        flist = ['%s.%04i' %(file_name, n) for n in nrange]
+        ds_sub = xr.merge([xr.open_mfdataset(file) for file in flist])
+        datasets.append(ds_sub)
+    out = xr.merge(datasets) # not sure why I have to do these seperately to work...
+    out.attrs['jb_notes'] = 'Combined using xarray by Julius Busecke. See https://github.com/jbusecke/mom6_design for details'
+    return out
 
 # Padding missing values
-def pad_ice_model(ds):
-    """pad the stupid masked out values in the restart files"""
-    def fill_missing(data, fillval):
-        return data.where(data<1e36).fillna(fillval)
-    
-    ds = ds.copy()
-    # Special treatmeant for the first class of `part_size`. There the fill value should be 1
-    part_size_1 = fill_missing(ds.part_size.isel(zaxis_1=0),1)
-    
-    ds_padded = fill_missing(ds, 0)
-    
-    temp = xr.concat([part_size_1, ds_padded.part_size.isel(zaxis_1=slice(1,None))], dim='zaxis_1')
-    ds_padded['part_size'] = temp
-    return ds_padded
-
 def transpose_like_ref(ds, ref):
     ds_trans = ds.copy()
     for dvar in ds.data_vars:
         ds_trans[dvar] = ds_trans[dvar].transpose(*ref[dvar].dims)
     return ds_trans
+
+def fill_missing(data, fillval):
+        return data.where(data<1e36).fillna(fillval)
+
+
+def pad_ice_model(ds):
+    """pad the stupid masked out values in the restart files"""
+    
+    ds = ds.copy()
+    
+    ds_padded = fill_missing(ds, 0)
+        
+    # Special treatmeant for the first class of `part_size`. There the fill value should be 1
+    part_size_1 = fill_missing(ds.part_size.isel(zaxis_1=0),1)
+    temp = xr.concat([part_size_1, ds_padded.part_size.isel(zaxis_1=slice(1,None))], dim='zaxis_1')
+    ds_padded['part_size'] = temp
+    ds_padded = transpose_like_ref(ds_padded, ds)
+    return ds_padded
